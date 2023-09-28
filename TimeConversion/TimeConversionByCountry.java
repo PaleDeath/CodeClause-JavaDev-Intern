bc6bb8753439c76c8d1b71459805055885305660
PaleDeath <113854365+PaleDeath@users.noreply.github.com>
2023-09-29 02:11:41 +0530

    Update TimeConversionByCountry.java

--- TimeConversion/TimeConversionByCountry.java
+++ TimeConversion/TimeConversionByCountry.java
@@ -1,11 +1,14 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.Scanner;
 
 public class TimeConversionByCountry {
-    // Constant for date and time format
-    public static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    // Constant for date, day, and time format
+    public static final String DATE_DAY_TIME_FORMAT = "yyyy-MM-dd EEE HH:mm:ss";
 
     // Constant for example time zones
     public static final String EXAMPLE_TIME_ZONES = "(e.g. Asia/Kolkata, Asia/Tashkent, Asia/Kuwait)";
@@ -54,16 +57,46 @@ public class TimeConversionByCountry {
 
                 // Convert local time to country time
                 LocalDateTime localDateTime = LocalDateTime.now(localZoneId);
-                LocalDateTime countryDateTime = localDateTime.atZone(localZoneId).withZoneSameInstant(countryZoneId).toLocalDateTime();
+                LocalDateTime countryDateTime = localDateTime.atZone(localZoneId)
+                        .withZoneSameInstant(countryZoneId)
+                        .toLocalDateTime();
 
-                // Formating Date and time
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_TIME_FORMAT);
-                String localTime = formatter.format(localDateTime);
-                String countryTime = formatter.format(countryDateTime);
+                // Show menu for customization
+                System.out.println("Options:");
+                System.out.println("1. Change time format");
+                System.out.println("2. Show date and day of the week");
+                System.out.println("3. Continue without customization");
+                System.out.print("Enter your choice: ");
+                int optionChoice = Integer.parseInt(sc.nextLine());
 
-                // Showing output
-                System.out.println("Local time (" + localZoneId + "): " + localTime);
-                System.out.println("Country time (" + countryZoneId + "): " + countryTime);
+                switch (optionChoice) {
+                    case 1:
+                        System.out.print("Enter the time format (e.g. 'yyyy-MM-dd HH:mm:ss' format): ");
+                        String timeFormat = sc.nextLine();
+                        DateTimeFormatter customFormatter = DateTimeFormatter.ofPattern(timeFormat);
+                        String localCustomTime = customFormatter.format(localDateTime);
+                        String countryCustomTime = customFormatter.format(countryDateTime);
+                        System.out.println("Local time (" + localZoneId + "): " + localCustomTime);
+                        System.out.println("Country time (" + countryZoneId + "): " + countryCustomTime);
+                        break;
+                    case 2:
+                        DateTimeFormatter dateDayFormatter = DateTimeFormatter.ofPattern(DATE_DAY_TIME_FORMAT);
+                        String localDateDayTime = dateDayFormatter.format(localDateTime);
+                        String countryDateDayTime = dateDayFormatter.format(countryDateTime);
+                        System.out.println("Local time (" + localZoneId + "): " + localDateDayTime);
+                        System.out.println("Country time (" + countryZoneId + "): " + countryDateDayTime);
+                        break;
+                    case 3:
+                        // Display default date and time format
+                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_DAY_TIME_FORMAT);
+                        String localTime = formatter.format(localDateTime);
+                        String countryTime = formatter.format(countryDateTime);
+                        System.out.println("Local time (" + localZoneId + "): " + localTime);
+                        System.out.println("Country time (" + countryZoneId + "): " + countryTime);
+                        break;
+                    default:
+                        System.out.println("Invalid choice");
+                }
 
                 // Ask the user if they want to continue using the program
                 System.out.print("Do you want to continue using this program? (yes/no): ");
@@ -72,7 +105,6 @@ public class TimeConversionByCountry {
                 if (continueResponse.equalsIgnoreCase("no")) {
                     continueProgram = false;
                     System.out.println("Thank you for using this program. Goodbye!");
-                    return;
                 }
             } catch (Exception e) {
                 // Displaying a friendly error message and asking the user to try again
